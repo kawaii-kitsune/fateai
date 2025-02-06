5c7159baf41ac32d699c481bca2138d779558717
Charalampos Kolokouris <c.kolokouris@adrestia.eu>
2025-02-06 16:57:35 +0200

    implement call for papers functionality; update hero section and add scientific committee data retrieval

--- routes/user/about.js
+++ routes/user/about.js
@@ -6,29 +6,37 @@ router.get('/', (req, res) => {
   const eventsQuery = 'SELECT * FROM events';
   const pricingQuery = 'SELECT * FROM pricing';
   const blogsQuery = 'SELECT * FROM blogs';
+  const committeeQuery = 'SELECT * FROM scientific_committee';
 
-//   connection.query(eventsQuery, (err, events) => {
-    // if (err) {
-    //   console.error('Error connecting to the database: ', err.stack);
-    //   return res.status(500).send('Error connecting to the database');
-    // }
+  connection.query(eventsQuery, (err, events) => {
+    if (err) {
+      console.error('Error connecting to the database: ', err.stack);
+      return res.status(500).send('Error connecting to the database');
+    }
 
-    // connection.query(pricingQuery, (err, pricing) => {
-    //   if (err) {
-    //     console.error('Error connecting to the database: ', err.stack);
-    //     return res.status(500).send('Error connecting to the database');
-    //   }
+    connection.query(pricingQuery, (err, pricing) => {
+      if (err) {
+        console.error('Error connecting to the database: ', err.stack);
+        return res.status(500).send('Error connecting to the database');
+      }
 
-    //   connection.query(blogsQuery, (err, blogs) => {
-    //     if (err) {
-    //       console.error('Error connecting to the database: ', err.stack);
-    //       return res.status(500).send('Error connecting to the database');
-    //     }
+      connection.query(blogsQuery, (err, blogs) => {
+        if (err) {
+          console.error('Error connecting to the database: ', err.stack);
+          return res.status(500).send('Error connecting to the database');
+        }
 
-        res.render('about', { });
-//       });
-//     });
-//   });
+        connection.query(committeeQuery, (err, committee) => {
+          if (err) {
+            console.error('Error connecting to the database: ', err.stack);
+            return res.status(500).send('Error connecting to the database');
+          }
+
+          res.render('about', { events, pricing, blogs, committee });
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
--- routes/user/cfp.js
+++ routes/user/cfp.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const connection = require('../../database/database');
+
 
 // Set up multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'assets/uploads/');
+    cb(null, 'assets/uploads/submitions');
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
@@ -16,15 +18,55 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.get('/', (req, res) => {
-  res.render('cfp');
+
+  const scientificCommitteeQuery = `
+    SELECT * FROM scientific_committee 
+    WHERE name IN (
+      'Konstantina Spanaki',
+      'Christos Floros',
+      'Constantin Zopounidis',
+      'Christos Lemonakis'
+    )
+  `;
+
+  connection.query(scientificCommitteeQuery, (err, results) => {
+      if (err) {
+          console.error('Error connecting to the database: ', err.stack);
+          return res.status(500).send('Error connecting to the database');
+      }
+
+      res.render('cfp', { sc: results });
+  });
 });
 
+
 router.post('/upload', upload.single('file'), (req, res) => {
+  const { name, university, email } = req.body;
+
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
-  res.send('File uploaded successfully.');
-});
 
+  const paperData = {
+    name: name,
+    university: university,
+    email: email,
+    filePath: req.file.path
+  };
+
+  const insertPaperQuery = `
+    INSERT INTO papers (name, university, email, file_path)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  connection.query(insertPaperQuery, [paperData.name, paperData.university, paperData.email, paperData.filePath], (err, results) => {
+    if (err) {
+      console.error('Error inserting paper data into the database: ', err.stack);
+      return res.status(500).send('Error saving paper data');
+    }
+
+    res.send('Paper submitted successfully.');
+  });
+});
 
 module.exports = router;
