80c5273fd70f2822b98d6195060e1df0f67b77d0
Charalampos Kolokouris <c.kolokouris@adrestia.eu>
2025-02-26 11:35:55 +0200

    feat: update layout and content for speakers and about sections

--- routes/user/speakers.js
+++ routes/user/speakers.js
@@ -3,31 +3,7 @@ const router = express.Router();
 // const connection = require('../../database/database');
 
 router.get('/', (req, res) => {
-
-  const speakersQuery = 'SELECT * FROM speakers';
-
-
-    connection.query(speakersQuery, (err, speakers) => {
-      if (err) {
-        console.error('Error connecting to the database: ', err.stack);
-        return res.status(500).send('Error connecting to the database');
-      }
-
-    //   connection.query(pricingQuery, (err, pricing) => {
-    //     if (err) {
-    //       console.error('Error connecting to the database: ', err.stack);
-    //       return res.status(500).send('Error connecting to the database');
-    //     }
-
-    //     connection.query(blogsQuery, (err, blogs) => {
-    //       if (err) {
-    //         console.error('Error connecting to the database: ', err.stack);
-    //         return res.status(500).send('Error connecting to the database');
-    //       }
-
-          res.render('speakers', { speakers });
-        });
-
+          res.render('speakers');
 });
 
 router.get ('/:id', (req, res) => {
